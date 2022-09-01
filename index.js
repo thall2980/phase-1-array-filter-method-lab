3dedbafe97523d9b75cddbb4d10067f3aed5a53f
Tyler Hall <thall2980@gmail.com>
2022-09-01 10:47:57 -0400

    commit tree ac7859bfc8dbc4a2d77b88ec8493979763c44549

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+//Code your solution here
+// let findMatching = (drivers) => {
+//     const result = drivers.find(match => match.name === drivers.name)
+//     return result.name ? (result.name) : undefined
+//}
+
+// const findMatching = drivers.filter(function (driver) {
+//     if (driver.name === driver.name);
+//     return driver.name
+// })
+
+// function findMatching(driver) {
+//     return driver.name
+// }
+
+function findMatching(drivers, element) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === element.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, element) {
+    return drivers.filter(function (driver) {
+        return driver.substring(0, element.length) === element
+    })
+}
+
+function matchName(drivers, element) {
+    return drivers.filter(function (driver) {
+        return driver.name === element
+    })
+}
